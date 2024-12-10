a92c509170262c7f9335dda0708db99e0ef3528e
bktang1 <166488376+bktang1@users.noreply.github.com>
2024-12-09 18:55:04 -0800

    Add files via upload

--- projectui.py
+++ projectui.py
@@ -24,7 +24,7 @@ def inject_custom_css():
 
         /* Welcome page background */
         .welcome-page {
-            background-image: url('file://{laboratory-equipment.jpeg}'); /* Replace with your image URL */
+            background-image: url('https://imgur.com/a/dTlqCP1'); /* Replace with your image URL */
             background-size: cover;
             background-repeat: no-repeat;
             background-attachment: fixed;
